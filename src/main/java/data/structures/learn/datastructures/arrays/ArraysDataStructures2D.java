65e9c6a9eb210a94e28fdeba4b0e8ea187a5793e
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-10-17 07:35:08 -0400

    defaults array output using deepToString()

--- src/main/java/data/structures/learn/datastructures/arrays/ArraysDataStructures2D.java
+++ src/main/java/data/structures/learn/datastructures/arrays/ArraysDataStructures2D.java
@@ -9,20 +9,15 @@ public class ArraysDataStructures2D {
 
         for (int i=0; i<3; i++) {
             for (int j=0; j<3; j++) {
-                board[i][j] = '-';// note that we are filling the board with a default value of dash using single quated because it;s char data type.
+                board[i][j] = '-';// default value as dash... outputs: [[-, -, -], [-, -, -], [-, -, -]] when called
 
             }
         }
         // Print Array
-        System.out.println(Arrays.toString(board));// Do not use this to ouput a 2D Array
-        System.out.println(Arrays.deepToString(board));// always use deepToString to output a 2D array.,,, /note it outputs the default dashes passed in
-        // Below is an assumption of the 3D where we have each box filled with a dash as default value...
-        System.out.println("___________________");
-        System.out.println("|  -  |  -  |  -  |");
-        System.out.println("___________________");
-        System.out.println("|  -  |  -  |  -  |");
-        System.out.println("___________________");
-        System.out.println("|  -  |  -  |  -  |");
-        System.out.println("___________________");
+
+        System.out.println(Arrays.deepToString(board));// use deepToString() for a 2D array to output correctly instead of toString()
+        //outputs: [[-, -, -], [-, -, -], [-, -, -]]
+
+
     }
 }
