9f0a43b761fda568da8aa1551873776fe068fc3d
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-10-17 12:10:45 -0400

    list of varios datatypes.

---
+++ src/main/java/data/structures/learn/datastructures/arrays/ListDataStructureArrayList1.java
@@ -0,0 +1,19 @@
+package data.structures.learn.datastructures.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDataStructureArrayList1 {
+    public static void main(String[] args) {
+
+        List colors = new ArrayList();// This works for everything thing.
+        colors.add("Purple");
+        colors.add("blue");
+        colors.add(1);
+        colors.add(new Object());
+
+
+        System.out.println(colors);
+
+    }
+}
