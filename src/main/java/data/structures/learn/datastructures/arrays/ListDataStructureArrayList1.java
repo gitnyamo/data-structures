4a9ab04db5691ca2974fd22ddd8b3278f87d4c47
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-10-17 12:32:02 -0400

    different way of looping through a list.

--- src/main/java/data/structures/learn/datastructures/arrays/ListDataStructureArrayList1.java
+++ src/main/java/data/structures/learn/datastructures/arrays/ListDataStructureArrayList1.java
@@ -9,9 +9,44 @@ public class ListDataStructureArrayList1 {
         List<String> colors = new ArrayList<>();// To work with a certain type make sure you pass it.
         colors.add("Purple");
         colors.add("blue");
+        colors.add("yellow");
 
 
-        System.out.println(colors);
+        System.out.println(colors.size());// outputs: 3
+        System.out.println(colors.contains("yellow"));// outputs: true
+        System.out.println(colors.contains("pink"));// outputs: false
+        System.out.println();
+        System.out.println("Below is the array output of a list");
+        System.out.println(colors);// outputs: [Purple, blue, yellow]
+        System.out.println();
+
+        System.out.println(" different ways to Loop through the colors: ");
+        System.out.println();
+
+        // 1st way to loop in a list...looking or looping through the arraylist
+        //loop using for each... regular.
+        System.out.println("Regular for each");
+        for (String color : colors) {// for each color print in a new line the color.
+            System.out.println(color);/* outputs: Purple
+                                                  blue
+                                                  yellow
+ */
+
+        }
+        //2nd way you can loop through a lit using the stream foreach....
+        System.out.println("loop using for each stream...");
+        colors.forEach(System.out::println);
+
+        //3rd way (to loop through a list using the get() and passing the counter to get the list using indices
+        System.out.println();
+        System.out.println("Loop using traditional get()");
+        for (int i = 0; i < colors.size(); i++){
+            System.out.println(colors.get(i));// use dot get index then pass i inside to get the list.
+        }
 
     }
+
+    // Note: All the 3 above ways gives the same results.... just choose one that is  more efficient for future scalability
+    // and considering your use case too.
+
 }
