7f4f14708af4f98e21f7fe7674655db94e986e76
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-10-17 05:45:23 -0400

    data struct

---
+++ src/main/java/data/structures/learn/datastructures/DataStructuresApplication.java
@@ -0,0 +1,13 @@
+package data.structures.learn.datastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructuresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/data/structures/learn/datastructures/DataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package data.structures.learn.datastructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
