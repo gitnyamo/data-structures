3f01d3e23c55b1197fb8830f097dbcf98de586c7
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-10-17 06:21:16 -0400

    arraysIndexOutOfBoundsException...

--- src/main/java/data/structures/learn/datastructures/arrays/ArrayDataStructure.java
+++ src/main/java/data/structures/learn/datastructures/arrays/ArrayDataStructure.java
@@ -18,11 +18,14 @@ public class ArrayDataStructure {
         System.out.println(Arrays.toString(colors));// outputs: [purple, blue, green, null, null]
 
 
-        int[] numbers = new int[2];
-        numbers[0] = 100;
-        numbers[1] = 200;
+        int[] numbers = {100, 200};// use this example when you know the elements your array is going to have
 
         System.out.println(Arrays.toString(numbers));
 
+        System.out.println(numbers[1]);// accessing the 2nd element of the array.... outputs works upto here with no exception
+
+        numbers[2] = 300;// assigning this to the array, and calling it will give you an ArrayIndexOutOfBoundsException
+
+        System.out.println(Arrays.toString(numbers));// Calling this gives you exception since the array was initially assigned oly to 2 elements.. Array are fixed
     }
 }
