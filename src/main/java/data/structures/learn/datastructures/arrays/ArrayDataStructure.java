93c9d284e7208e04f42cabfafaab7aa64eeedaac
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-10-17 06:24:27 -0400

    loop through arrays and in reverse too

--- src/main/java/data/structures/learn/datastructures/arrays/ArrayDataStructure.java
+++ src/main/java/data/structures/learn/datastructures/arrays/ArrayDataStructure.java
@@ -15,17 +15,13 @@ public class ArrayDataStructure {
 
         colors[2] = "green";
 
-        System.out.println(Arrays.toString(colors));// outputs: [purple, blue, green, null, null]
+        System.out.println(Arrays.toString(colors));
 
 
-        int[] numbers = {100, 200};// use this example when you know the elements your array is going to have
+        int[] numbers = {100, 200};
 
         System.out.println(Arrays.toString(numbers));
 
-        System.out.println(numbers[1]);// accessing the 2nd element of the array.... outputs works upto here with no exception
-
-        numbers[2] = 300;// assigning this to the array, and calling it will give you an ArrayIndexOutOfBoundsException
-
-        System.out.println(Arrays.toString(numbers));// Calling this gives you exception since the array was initially assigned oly to 2 elements.. Array are fixed
+        System.out.println(numbers[1]);
     }
 }
